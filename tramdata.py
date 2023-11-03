b6c3f68a6c861a1e8bd381da881dd356f68883ce
MarSchw <marcelsc@chalmers.se>
2023-11-03 14:46:36 +0100

    Update tramdata.py

--- tramdata.py
+++ tramdata.py
@@ -10,11 +10,17 @@ LINE_FILE = './data/tramlines.txt'
 TRAM_FILE = './tramnetwork.json'
 
 def build_tram_stops(jsonobject):
-    ## YOUR CODE HERE
-    pass
+    dict_position = {}
+    with open(jsonobject, 'r') as infile:
+        stop_data = json.load(infile)
+        for station in stop_data:
+            lat, long = stop_data.get(station).get('position')
+            dict_position[station] = {"lat": lat, "long": long}
+        return dict_position
+#print(build_tram_stops(STOP_FILE))
 
 def build_tram_lines(lines):
-    tram_lines = {}
+    '''tram_lines = {}
     
     with open(lines) as file:
         opened_file = file.readlines()
@@ -31,6 +37,35 @@ def build_tram_lines(lines):
                     else: name = name[0]
                     stops.append(name)
             tram_lines.setdefault(tramline,stops)
+        return tram_lines'''
+
+    tram_lines = {}
+    with open(lines, 'r') as file:
+        opened_file = file.readlines()
+        A = True
+        while A:
+            for lines in opened_file:
+                stops = []
+                if lines[0].isdigit():
+                    tramline = lines.strip('\n').replace(":","")
+                    tram_lines.setdefault(tramline,stops)
+                '''if lines[0].isdigit() or lines[0:1].isdigit():
+                    lines = lines.strip("\n")
+                    lines = lines.replace(":", "")
+                    if len(lines) == 1:
+                        tramline = str(lines[0])
+                    elif len(lines) == 2:
+                        tramline = str(lines[0:2])
+                    tram_lines.setdefault(tramline, stops)'''
+                if lines[0].isalpha():
+                    lines = lines.strip('\n').split()
+                    name = lines[:-1]
+                    if len(name) == 2:
+                        name = " ".join(name)
+                    else: name = name[0]
+                    tram_lines[tramline].append(name)
+                if lines[0] in ['\n', ' ']:
+                    A = False
         return tram_lines
 print(build_tram_lines(LINE_FILE))
 
