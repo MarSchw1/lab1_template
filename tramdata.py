555ac638c903fd88b2ab7b6eea1934473a255b04
MarSchw <marcelsc@chalmers.se>
2023-11-04 11:03:33 +0100

    Update tramdata.py

--- tramdata.py
+++ tramdata.py
@@ -10,27 +10,30 @@ LINE_FILE = './data/tramlines.txt'
 TRAM_FILE = './tramnetwork.json'
 
 def build_tram_stops(jsonobject):
-    dict_position = {}
+    stopdict = {}
     with open(jsonobject, 'r') as infile:
         stop_data = json.load(infile)
         for station in stop_data:
             lat, long = stop_data.get(station).get('position')
-            dict_position[station] = {"lat": lat, "long": long}
-        return dict_position
+            stopdict[station] = {"lat": lat, "long": long}
+        return stopdict
 #print(build_tram_stops(STOP_FILE))
 
 def build_tram_lines(lines):
-    tram_lines = {}
-    time_dict = {}
+    linedict = {}
+    timedict = {}
+    temp_timedict = {}
     with open(lines, 'r',encoding="utf-8") as file:
         opened_file = file.readlines()
         A = True
         while A:
             for lines in opened_file:
                 stops = []
+                times = {}
                 if lines[0].isdigit():
                     tramline = lines.strip('\n').replace(":","")
-                    tram_lines.setdefault(tramline,stops)
+                    linedict.setdefault(tramline,stops)
+                    temp_timedict.setdefault(tramline,times)
                 if lines[0].isalpha():
                     lines = lines.strip('\n').split()
                     name = lines[:-1]
@@ -38,12 +41,18 @@ def build_tram_lines(lines):
                     if len(name) != 1:
                         name = " ".join(name)
                     else: name = name[0]
-                    tram_lines[tramline].append(name)
+                    linedict[tramline].append(name)
+                    temp_timedict[tramline].setdefault(name,time)
                 if lines[0] in ['\n', ' ']:
                     A = False
-        return tram_lines
-print(build_tram_lines(LINE_FILE))
+        #return tram_lines
+        return temp_timedict
+    build_timedict(temp_timedict)
+build_tram_lines(LINE_FILE)
 
+def build_timedict(temp_timedict):
+    timedict = {}
+    print(temp_timedict)
 
 def build_tram_network(stopfile, linefile):
     ## YOUR CODE HERE
