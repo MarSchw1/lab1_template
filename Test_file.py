bcf34ee0c6627439e64e61f49e88ab0c8d9407fb
MarSchw <marcelsc@chalmers.se>
2023-11-06 11:26:37 +0100

    Update Test_file.py

--- Test_file.py
+++ Test_file.py
@@ -7,46 +7,45 @@ STOP_FILE = './data/tramstops.json'
 LINE_FILE = './data/tramlines.txt'
 
 def build_tram_lines(lines):
-    tram_lines = {}
-    with open(lines) as f:
-        opened_file = f.readlines()
-        for line in opened_file:
-            line = line.strip('\n')
-        while line != ' ':
-            stops = []
-            if line[0].isdigit():
-                number = line.replace(':','')
-            if line[0].isalpha():
-                lines = lines.strip('\n').split()
-                name = lines[:-1]
-                if len(name) == 2:
-                    name = " ".join(name)
-                else: name = name[0]
-                stops.append(name)
-            tram_lines.setdefault(number,stops)
-        return tram_lines
-#print(build_tram_lines(LINE_FILE))
-
-# Detta är ett äldre försök som inte fungerar som det ska. Vissa delar gör dock det.
-'''tram_lines = {}
-    
-    with open(lines) as file:
+    linedict = {}
+    timedict = {}
+    temp_timedict = {}
+    stops = []
+    with open(lines, 'r',encoding="utf-8") as file:
         opened_file = file.readlines()
-        while opened_file != '\n':
-            stops = []
-            for i in opened_file:
+        A = True
+        for i in range(len(opened_file)):
+            while A:
+                stops = []
+                times = {}
                 if i[0].isdigit():
                     tramline = i.strip('\n').replace(":","")
+                    linedict.setdefault(tramline,stops)
                 if i[0].isalpha():
                     i = i.strip('\n').split()
                     name = i[:-1]
-                    if len(name) == 2:
+                    time = i[-1]
+                    if len(name) != 1:
                         name = " ".join(name)
                     else: name = name[0]
-                    stops.append(name)
-            tram_lines.setdefault(tramline,stops)
-        return tram_lines'''
+                    linedict[tramline].append(name)
+                    temp_timedict[tramline].setdefault(name,time)
+                if i[0] in ['\n', ' ']:
+                    A = False
+                '''for tramline in temp_timedict:
+                    stops = list(temp_timedict[tramline].keys())
+                for i in range(len(stops)-1):
+                    current_stop = stops[i]
+                    next_stop = stops[i+1]
+                    current_time = int(temp_timedict[tramline][current_stop].replace(':',''))
+                    next_time = int(temp_timedict[tramline][next_stop].replace(':',''))
+                    diff_time = next_time - current_time 
+                    diff_dict = {}
+                    timedict.setdefault(current_stop,diff_dict)
+                    timedict[current_stop].setdefault(next_stop,diff_time)'''
 
-print(int('10:00') - int('10:02'))
+    return linedict, timedict
+print(build_tram_lines(LINE_FILE))
+#build_tram_lines(LINE_FILE)
 
 
