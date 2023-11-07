84ff16958daab940ee1dd440c56fc1b4855163e4
MarSchw <marcelsc@chalmers.se>
2023-11-07 22:13:31 +0100

    Update Test_file.py

--- Test_file.py
+++ Test_file.py
@@ -30,6 +30,8 @@ def build_tram_lines(lines):
     with open(lines, 'r',encoding="utf-8") as file:
         opened_file = file.readlines()
         A = True
+        prev_station = None
+        prev_time = None
         while A:
             for lines in opened_file:
                 stops = []
@@ -41,17 +43,19 @@ def build_tram_lines(lines):
                     lines = lines.strip('\n').split()
                     stop_name = lines[:-1]
                     if len(stop_name) != 1:
-                        name = " ".join(stop_name)
+                        stop_name = " ".join(stop_name)
                     else: stop_name = stop_name[0]
                     line_dict[tramline].append(stop_name)
 
-                    prev_station = False
+                    
                     time = int((lines[-1]).replace(':',''))
-                    if prev_station not in time_dict[prev_station] and prev_station:
-                        time_diff = time - prev_time
-                        time_dict.setdefault(prev_station,{}).setdefault(stop_name, time_diff)
-                    prev_station, prev_time = stop_name, time
-
+                    if prev_station is not None:
+                        if prev_station not in time_dict.get(prev_station, {}) and prev_station:
+                            time_diff = time - prev_time
+                            time_dict.setdefault(prev_station,{}).setdefault(stop_name, time_diff)
+                        prev_station, prev_time = stop_name, time
+                    if prev_station is None:
+                        prev_station, prev_time = stop_name, time
                 if lines[0] in ['\n', ' ']:
                     A = False
                          
