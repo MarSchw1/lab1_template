fef73541be3aeb095ab0dd2596026256ffc5044b
MarSchw <marcelsc@chalmers.se>
2023-11-07 11:25:56 +0100

    Update Test_file.py

--- Test_file.py
+++ Test_file.py
@@ -9,12 +9,31 @@ def build_lines(linefile):
     linedict = {}
     timedict = {}
     with open(linefile, 'r', encoding='utf-8') as file:
-        lines = file.read().split('\n') 
-        print(lines)
+        tramlines = file.read().split('\n\n') 
+        #print(len(tramlines))
+        for line in tramlines:
+            line = line.split('\n')
+            stops = []
+            for i,e in enumerate(line):
+                if len(e) == 2 or len(e) == 3:
+                    tramNr = e.strip(':')
+                    linedict.setdefault(tramNr, stops)
+                
+                elif e is not None:
+                    e = e.split()
+                    stopName = ' '.join(e[:-1])
+                    time = e[-1].replace(':','')
+                    linedict[tramNr].append(stopName)
 
 
-build_lines(LINE_FILE)
-        
+                    
+
+
+                
+        return linedict
+
+print(build_lines(LINE_FILE))
+#build_lines(LINE_FILE)       
 
 
 
