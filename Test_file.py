67d291d5abc09ff0acb879b02c3504f784766b00
MarSchw <marcelsc@chalmers.se>
2023-11-07 21:55:11 +0100

    Update Test_file.py

--- Test_file.py
+++ Test_file.py
@@ -5,7 +5,7 @@ import json
 # files given
 STOP_FILE = './data/tramstops.json'
 LINE_FILE = './data/tramlines.txt'
-def build_lines(linefile):
+'''def build_lines(linefile):
     linedict = {}
     timedict = {}
     with open(linefile, 'r', encoding='utf-8') as file:
@@ -23,16 +23,41 @@ def build_lines(linefile):
                     e = e.split()
                     stopName = ' '.join(e[:-1])
                     time = e[-1].replace(':','')
-                    linedict[tramNr].append(stopName)
+                    linedict[tramNr].append(stopName)'''
+def build_tram_lines(lines):
+    line_dict = {}
+    time_dict = {}
+    with open(lines, 'r',encoding="utf-8") as file:
+        opened_file = file.readlines()
+        A = True
+        while A:
+            for lines in opened_file:
+                stops = []
+                times = {}
+                if lines[0].isdigit():
+                    tramline = lines.strip('\n').replace(":","")
+                    line_dict.setdefault(tramline,stops)
+                if lines[0].isalpha():
+                    lines = lines.strip('\n').split()
+                    stop_name = lines[:-1]
+                    if len(stop_name) != 1:
+                        name = " ".join(stop_name)
+                    else: stop_name = stop_name[0]
+                    line_dict[tramline].append(stop_name)
 
+                    prev_station = False
+                    time = int((lines[-1]).replace(':',''))
+                    if prev_station not in time_dict[prev_station] and prev_station:
+                        time_diff = time - prev_time
+                        time_dict.setdefault(prev_station,{}).setdefault(stop_name, time_diff)
+                    prev_station, prev_time = stop_name, time
 
-                    
+                if lines[0] in ['\n', ' ']:
+                    A = False
+                         
+        return time_dict
 
-
-                
-        return linedict
-
-print(build_lines(LINE_FILE))
+print(build_tram_lines(LINE_FILE))
 #build_lines(LINE_FILE)       
 
 
